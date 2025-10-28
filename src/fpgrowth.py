6869d708ecc6051ed62812a655b86cca274f0ed5
atta238 <2023170052@cis.asu.edu.eg>
2025-10-29 01:09:09 +0300

    Step 2:use frequent_items to re-arrange items in transactionTable

--- src/fpgrowth.py
+++ src/fpgrowth.py
@@ -44,6 +44,31 @@ print(frequent_items)
 
 # TODO (Step 2): use frequent_items to re-arrange items in transactionTable
 arrangedTable = [] # put the result here
+item_order = {}
+index = 0
+for pair in frequent_items:
+    item_order[pair[0]] = index
+    index+=1
+
+for transaction in transactionTable:
+
+    filtered_items = []
+
+    for item in transaction:
+       if item in item_order:
+          filtered_items.append(item)
+
+
+    sorted_items = []
+
+    for pair in frequent_items:
+        item_name = pair[0]
+        if item_name in filtered_items:
+            sorted_items.append(item_name)
+
+
+    if sorted_items:
+        arrangedTable.append(sorted_items)
 
 # TODO (Step 3): generate the tree based on arrangedTable
 root = FPNode(None, None, None) # this is the root, keep everything None except children
