6a80f825bb54797dcb1192aedb58a36454d78172
Menna Hisham <mennahisham2050@gmail.com>
2025-11-13 22:03:07 +0200

    add conditional trees, fix frequent patterns and add support counts

--- src/fpgrowth.py
+++ src/fpgrowth.py
@@ -95,9 +95,9 @@ for transaction in arrangedTable:
         
 
 all_paths = {}
-for item, nodes in frequent_nodes.items():
+for item, node in frequent_nodes.items():
     all_paths[item] = []
-    for node in nodes:
+    for node in node:
         path = []
         temp = node
         depth = 0
@@ -124,10 +124,10 @@ for item in items_order:
 
 print("===============================")
 
-# TODO (Step 4): traverse the tree starting from least frequent characters (use frequent_items and frequent_nodes to find the starting nodes)
+# TODO (Step 4): traverse the tree starting from least frequent characters (use frequent_items and frequent_nodes to find the starting node)
 # for each starting node, traverse the tree by going to the parent until you hit the root
-# generate the conditional trees (sets of the nodes in the path that leads up to that node) 
-conditional_trees = {}
+# generate the conditional trees (sets of the node in the path that leads up to that node) 
+conditional_pattern_base = {}
 for item in reversed(frequent_items):
     for node in frequent_nodes[item[0]]:
         if (node is root):
@@ -138,12 +138,12 @@ for item in reversed(frequent_items):
         while (temp is not root):
             conditional.append(temp.item)
             temp = temp.parent
-        if (node.item not in conditional_trees ):
-            conditional_trees[node.item] = []
-        conditional_trees[node.item].append((conditional,frequency))
+        if (node.item not in conditional_pattern_base):
+            conditional_pattern_base[node.item] = []
+        conditional_pattern_base[node.item].append((conditional, frequency))
 
 print("CONDITIONAL BASE PATTERNS\n===============================")
-for key, paths in conditional_trees.items():
+for key, paths in conditional_pattern_base.items():
     print(f"{key}:")
     for path, freq in paths:
         print(f"  {path}:{freq}")
@@ -153,40 +153,59 @@ print("===============================")
 
 # TODO (Step 5): find the frequent patterns from the conditional trees
 # note that unlike in the lecture and lab examples, the given conditional trees INCLUDE the node itself, it'll always be the first element
-frequent_patterns = []
-minimum_support_count = minimumSupport * len(transactionTable)
 
-for key, paths in conditional_trees.items():
+frequent_patterns = []
+minimum_support_count = int(minimumSupport * len(transactionTable))
+
+for key, paths in conditional_pattern_base.items():
+    accepted_patterns = []
+    accepted_nodes = []
+    frequent_patterns_set = set()
     node_frequency = Counter()
-    accepted_nodes= [key]
+
+    print(f"ITEM -> {key}:")
 
     for path, count in paths:
         for node in path:
             node_frequency[node] += count
 
-    for node, freq in node_frequency.items():
+    print(f"CONDITIONAL TREE:")
+
+    for node,freq in node_frequency.items():
         if freq >= minimum_support_count:
-            accepted_nodes.append(node)
+            accepted_patterns.append((node,freq))
+            accepted_nodes.append (node)
+            print(f"{node} : {freq}")
 
-    if(len(accepted_nodes) > 1):
-        for length in range(2, len(accepted_nodes) + 1):
-            for combination in (itertools.combinations(accepted_nodes, length)):
-                if list(combination) not in frequent_patterns:
-                    frequent_patterns.append(list(combination))
+    for length in range(1, len(accepted_nodes)+1):
+        for combination in combinations(accepted_nodes, length):
+            possible_pattern = tuple(sorted(list(combination) + [key]))
+            possible_pattern_set = set(combination)
+            possible_pattern_support = 0
 
-print("FREQUENT PATTERNS\n===============================")
-print(frequent_patterns)
-print("===============================")
+            for path, count in paths:
+                if possible_pattern_set.issubset(set(path)):
+                    possible_pattern_support += count
 
+            if possible_pattern_support >= minimum_support_count:
+                frequent_patterns_set.add((possible_pattern,possible_pattern_support))
+
+    print("FREQUENT PATTERNS:")
+
+    for frequent_pattern, support in frequent_patterns_set:
+        print(f"{list(frequent_pattern)}:{support}")
+        frequent_patterns.append((list(frequent_pattern),support))
+
+    print("===============================")
 # TODO (Step 6): for each frequent pattern, generate all possible subsets, excluding the empty subset and the complete subset:  
 #                   - For each subset:  
 #                       - Find the complementary subset and calculate association rules.  
 #                       - Extract strong rules based on the minimum confidence threshold.
 #                   - Calculate lift for every strong rule
 
-strong_rules = [] # put the result here 
-for patteren in frequent_patterns:
-    items =set (patteren)
+strong_rules = [] # put the result here
+for pattern in frequent_patterns:
+    items =set (pattern)
     if len(items)<2:
         continue
     for i in range (1,len(items)):
@@ -198,10 +217,10 @@ for patteren in frequent_patterns:
 
         support_both=sum(1 for t in transactionTable if items.issubset(t))/len(transactionTable)
         support_left=sum(1 for t in transactionTable if subset.issubset(t))/len(transactionTable)
-        support_righ=sum(1 for t in transactionTable if remain.issubset(t))/len(transactionTable)
-        if support_left >0 and support_righ >0 :
+        support_right= sum(1 for t in transactionTable if remain.issubset(t)) / len(transactionTable)
+        if support_left >0 and support_right >0 :
          confidence =(support_both/support_left)
-         lift =confidence/support_righ
+         lift = confidence / support_right
          if confidence>=minimumConfidence:
             strong_rules.append(
                 {
